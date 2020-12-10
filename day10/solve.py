d9f61fde36f9479fb364f7ad652e2295b745a5cf
BobSaget <36662130+andreaslillevangbech@users.noreply.github.com>
2020-12-10 19:13:08 +0100

    day10

---
+++ day10/solve.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+dat = open('input').read().strip().split('\n')
+dat = sorted([int(x) for x in dat])
+dat.append(max(dat)+3)
+
+lst = []
+a = 0
+for i in dat:
+    if i-a>3 or i-a<1:
+        print("incompatible")
+        break
+    lst.append(i-a)
+    a = i
+print(lst.count(1) *  lst.count(3))
+
+print("Part 2")
+
+dat = [0] + dat
+v= {dat[-1]: 1}
+for i in reversed(dat[:-1]):
+    v[i] = 0
+    for key in v:
+        if 0<key-i<4:
+            v[i] += v[key]
+print(v[0])
+
